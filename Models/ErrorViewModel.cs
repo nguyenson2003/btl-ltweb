83812381fcc82e8a0887ea1f6f8cc391848a7d00
nguyenson2003 <59283327+nguyenson2003@users.noreply.github.com>
2023-10-25 14:57:54 +0700

    push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace btl_tkweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
