0c3eb77ea5beae3247a543f15f05f4d7d2729586
nguyenson2003 <59283327+nguyenson2003@users.noreply.github.com>
2023-10-29 00:39:37 +0700

    giaovien

---
+++ Controllers/MonHocController.cs
@@ -0,0 +1,19 @@
+﻿using btl_tkweb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btl_tkweb.Controllers
+{
+    public class MonHocController : Controller
+    {
+        SchoolContext db;
+        public MonHocController(SchoolContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var monhoc = db.MonHoc.ToList();
+            return View(monhoc);
+        }
+    }
+}
--- Data/DbInit.cs
+++ Data/DbInit.cs
@@ -14,7 +14,10 @@ namespace btl_tkweb.Data
                 {
                     return;
                 }
-                var lop = new Lop[] { new Lop { LopID = "10A1", GVCN = "Nguyen Thi A" } };
+                var lop = new Lop[] { 
+                    new Lop { LopID = "10A1", GVCN = "Nguyen Thi A" },
+                    new Lop { LopID = "10A2", GVCN = "Nguyen Thi X" },
+                };
                 foreach (var x in lop)
                 {
                     context.Lop.Add(x);
@@ -45,11 +48,27 @@ namespace btl_tkweb.Data
                 context.SaveChanges();
                 var giaovien = new GiaoVien[]
                 {
-                    new GiaoVien(){HoVaTen="Nguyễn Thị A", Nu=true,NgaySinh = DateTime.Parse("1990-01-01"),MonHocID=1}
+                    new GiaoVien(){HoVaTen="Nguyễn Thị A", Nu=true,NgaySinh = DateTime.Parse("1990-01-01"),MonHocID=1},
+                    new GiaoVien(){HoVaTen="Nguyễn Thị X", Nu=true,NgaySinh = DateTime.Parse("1990-01-01"),MonHocID=2},
+                    new GiaoVien(){HoVaTen="Nguyễn Thị Y", Nu=true,NgaySinh = DateTime.Parse("1990-01-01"),MonHocID=3},
+                    new GiaoVien(){HoVaTen="Nguyễn Thị Z", Nu=true,NgaySinh = DateTime.Parse("1990-01-01"),MonHocID=2},
                 };
                 foreach (var x in giaovien)
                 {
                     context.GiaoVien.Add(x);
+                }
+                context.SaveChanges();
+                var ctgd = new ChiTietGiangDay[]
+                {
+                    new ChiTietGiangDay(){LopHocId="10A1", GiaoVienID=1},
+                    new ChiTietGiangDay(){LopHocId="10A1", GiaoVienID=2},
+                    new ChiTietGiangDay(){LopHocId="10A1", GiaoVienID=3},
+                    new ChiTietGiangDay(){LopHocId="10A2", GiaoVienID=1},
+                    new ChiTietGiangDay(){LopHocId="10A2", GiaoVienID=4},
+                };
+                foreach (var x in ctgd)
+                {
+                    context.ChiTietGiangDay.Add(x);
                 }
                 context.SaveChanges();
             }
