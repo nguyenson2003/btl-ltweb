f7025cac4063f64c5f339b2862b93409cb455f34
khoaaaaaaz <149084884+khoaaaaaaz@users.noreply.github.com>
2023-10-30 22:36:21 +0700

    Create DiemSo.cs

---
+++ Controllers/DiemSo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btl_tkweb.Controllers
+{
+    public class DiemSo : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
