00d2257a010a133c887c3a66820510d6cbfb553e
nguyenson2003 <59283327+nguyenson2003@users.noreply.github.com>
2023-10-30 12:05:31 +0700

    giao vien

--- Controllers/GiaoVienController.cs
+++ Controllers/GiaoVienController.cs
@@ -17,10 +17,22 @@ namespace btl_tkweb.Controllers
         public IActionResult Index()
         {
             var gv = db.GiaoVien.Include(s => s.MonHoc).ToList();
+            var lop = new List<Lop>();
+            lop.Add(new Lop() { LopID = "" });
+            lop.AddRange(db.Lop);
+            ViewBag.lopId = new SelectList(lop, "LopID","LopID","");
+
+            var mon= new List<MonHoc>();
+            mon.Add(new MonHoc() { MonHocID= 0 ,TenMon="--",HeSoMon=0});
+            mon.AddRange(db.MonHoc);
+            ViewBag.MonHoc = new SelectList(mon, "MonHocID", "TenMon", "--");
+
             return View(gv);
         }
-        public IActionResult Table(string LopID, int? MonHocID)
+        public PartialViewResult Table(string LopID, int? MonHocID)
         {
+            if (LopID == "--") LopID = null;
+            if(MonHocID==0)MonHocID=null;
             if (LopID == null && MonHocID == null)
             {
                 var gv = db.GiaoVien.Include(s => s.MonHoc).ToList();
@@ -28,17 +40,17 @@ namespace btl_tkweb.Controllers
             }
             else if (LopID == null)
             {
-                var gv = db.GiaoVien.Include(s => s.MonHoc).Where(s => s.MonHocID == MonHocID);
+                var gv = db.GiaoVien.Include(s => s.MonHoc).Where(s => s.MonHocID == MonHocID).ToList();
                 return PartialView(gv);
             }
             else if (MonHocID == null)
             {
-                var gv = db.GiaoVien.Include(s => s.MonHoc).Include(s => s.ctgd).Where(s => s.ctgd.Any(c => c.LopHocId == LopID));
+                var gv = db.GiaoVien.Include(s => s.MonHoc).Include(s => s.ctgd).Where(s => s.ctgd.Any(c => c.LopHocId == LopID)).ToList();
                 return PartialView(gv);
             }
             else
             {
-                var gv = db.GiaoVien.Include(s => s.MonHoc).Include(s => s.ctgd).Where(s => s.ctgd.Any(c => c.LopHocId == LopID)).Where(s => s.MonHocID == MonHocID);
+                var gv = db.GiaoVien.Include(s => s.MonHoc).Include(s => s.ctgd).Where(s => s.ctgd.Any(c => c.LopHocId == LopID)).Where(s => s.MonHocID == MonHocID).ToList();
                 return PartialView(gv);
             }
         }
