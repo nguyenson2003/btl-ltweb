0dca644812bc170e72c0cbff7ce4d527acd9fec5
khoaaaaaaz <149084884+khoaaaaaaz@users.noreply.github.com>
2023-10-30 23:13:08 +0700

    monhocnaynhucl

---
+++ Controllers/DiemSoController.cs
@@ -0,0 +1,24 @@
+﻿using btl_tkweb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace btl_tkweb.Controllers
+{
+    public class DiemSoController : Controller
+    {
+        SchoolContext db;
+        public DiemSoController(SchoolContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index(String HocSinhID)
+        {
+            var bd = db.DiemSo.Include(l => l.HocSinh).Where(s => s.HocSinhId.Equals(HocSinhID)).ToList();
+            var hs = db.DiemSo.Find(HocSinhID);
+            ViewBag.HocSinh = hs.HocSinh.Ho + hs.HocSinh.Ten;
+            return View(bd);
+        }
+       
+    }
+}
