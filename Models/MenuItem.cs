75dbad782929ea5ea7c781b6ffbf10b676a2990a
khoaaaaaaz <149084884+khoaaaaaaz@users.noreply.github.com>
2023-11-02 22:22:59 +0700

    view  component

---
+++ Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace btl_tkweb.Models
+{
+    public class MenuItem
+    {
+        public int ItemID { get; set; }
+        public string? Name { get; set; }
+        public string? Link { get; set; }
+    }
+}
---
+++ ViewComponents/LeftMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using btl_tkweb.Data;
+using btl_tkweb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SQLitePCL;
+
+namespace btl_tkweb.ViewComponents
+{
+    public class LeftMenuViewComponent : ViewComponent
+    {
+        SchoolContext _context;
+        private List<MenuItem> MenuItems = new List<MenuItem>();
+        public LeftMenuViewComponent(SchoolContext context)
+        {
+            _context = context;
+
+           
+            
+        }
+
+        
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("DefaultLeftMenu", MenuItems);
+        }
+    }
+}
+ 
